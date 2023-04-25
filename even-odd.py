7abf5c60cf023334dc10abcad2171a29af11e788
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-25 16:46:43 +0800

    Initial commit

---
+++ even-odd.py
@@ -0,0 +1,14 @@
+# Ray Allessandra D. Pacis | BSCPE 1-5 
+
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+# The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
+# The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+# open numbers.txt , even.txt, and odd.txt
+
+    # read number.txt by line
+        # convert each line to integer
+        # if extracted number is even
+            # put extracted number to even.txt
+        # else if the extracted number is odd
+            # put extracted number to odd.txt

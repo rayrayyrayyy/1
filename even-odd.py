bdc6f728ccea32a627da0ae780e665827f2a4d5d
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-28 00:18:01 +0800

    design done

--- even-odd.py
+++ even-odd.py
@@ -1,5 +1,4 @@
 # Ray Allessandra D. Pacis | BSCPE 1-5 
-
 # Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
 # The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
 # The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
@@ -26,7 +25,7 @@ with open("numbers.txt", 'w') as input_file:
 
     # create loop until we have 20 integers
     i = 0
-    while (i < 2):
+    while (i < 20):
         # ask user for numbers
         random_numbers = input('\033[0;31m' + "\t\tPlease write a number: \033[0m")
         # put the numbers to numbers.txt
